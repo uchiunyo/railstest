6b9dc51b7907c5e024a2fb1266127c3bd92adc1e
uchi <uchi@uchi-ubuntu-main.(none)>
2012-12-09 00:57:19 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railbook4::Application.config.secret_token = 'b64337ee30d0eca3920eb3fed68d068e67dd7101ab7978ee6a592b4bb0726ed6beeb26ea01041b3870b3e92a80cabdf1ad4cce499b813027a4cba7f460dc5227'
